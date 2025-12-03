a68eb0fcf97d5799fcf10b40ac4ff47e5fbc424b
Great-Khans <186938001+Great-Khans@users.noreply.github.com>
2025-12-03 11:54:07 -0700

    forgot parentheses in length methods

--- StringOperations.java
+++ StringOperations.java
@@ -8,24 +8,24 @@ public class StringOperations{
         This program outputs the scrambled (first and last letters swapped) form of each word.*/
         String word = "popcorn";
         String fwordl = word.substring(0, 1);
-        String mworldl = word.substring(1, word.length - 1);
-        String lwordl = word.substring(word.length - 1);
+        String mworldl = word.substring(1, word.length() - 1);
+        String lwordl = word.substring(word.length() - 1);
         String scram = lwordl + mworldl + fwordl;
 
         System.out.println(scram);
 
         word = "picture";
         fwordl = word.substring(0, 1);
-        mworldl = word.substring(1, word.length - 1);
-        lwordl = word.substring(word.length - 1);
+        mworldl = word.substring(1, word.length() - 1);
+        lwordl = word.substring(word.length() - 1);
         scram = lwordl + mworldl + fwordl;
 
         System.out.println(scram);
 
         word = "computer";
         fwordl = word.substring(0, 1);
-        mworldl = word.substring(1, word.length - 1);
-        lwordl = word.substring(word.length - 1);
+        mworldl = word.substring(1, word.length() - 1);
+        lwordl = word.substring(word.length() - 1);
         scram = lwordl + mworldl + fwordl;
 
         System.out.println(scram);
